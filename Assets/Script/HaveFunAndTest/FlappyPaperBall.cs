4a1d8146633517dea42955309484057e3d2f16d8
frto027 <q602706150@sina.com>
2016-09-23 12:50:01 +0800

    Build 8
    增加部分物理机关……

---
+++ Assets/Script/HaveFunAndTest/FlappyPaperBall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyPaperBall : F_Object
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if(levelManager.Player == other.GetComponent<F_Player>() && levelManager.Player.PlayerType == F_Player.PlayerTypes.Paper)
+        {
+            levelManager.Player.GetComponent<Transform>().localScale = new Vector3(0.1f, 1, 1);
+        }
+    }
+}
